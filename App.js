72be3b125e559f59fb25ce48545641aea42c5cd2
Susanna <susanna.tuomi@myy.haaga-helia.fi>
2023-03-01 12:36:44 +0200

    Shoppinglist with Firebase

--- App.js
+++ App.js
@@ -1,11 +1,71 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, FlatList  } from 'react-native';
+
+import { push, ref, onValue, remove } from 'firebase/database';
+import React, { useState, useEffect } from 'react';
+
+import database from './firebase';
 
 export default function App() {
+
+  const [product, setProduct] = useState('');
+  const [amount, setAmount] = useState('');
+  const [list, setList] = useState([]);
+
+  useEffect(() => {
+    const itemsRef = ref(database, 'list/');
+    onValue(itemsRef, (snapshot) => {
+    const data = snapshot.val();
+    console.log(data)
+    const items = data ? Object.keys(data).map(key =>({key, ...data[key]})) : [];
+    console.log(items)
+    setList(items);
+    })
+    }, []);
+
+  const saveItem = () => {
+    push(
+      ref(database, 'list/'),
+      {'product': product, 'amount': amount})
+;  }
+
+const deleteItem = (key) => {
+  console.log('deleteItem', key);
+  remove(ref(database, 'list/' + key))
+};
+
+  const listSeparator = () => {
+    return (
+      <View
+        style={{
+          height: 5,
+          width: "80%",
+          backgroundColor: "#fff",
+          marginLeft: "10%"
+        }}
+      />
+    );
+  };
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput placeholder='Product' style={{marginTop: 30, fontSize: 18, width: 200, borderColor: 'gray', borderWidth: 1}}
+        onChangeText={(product) => setProduct(product)}
+        value={product}/>  
+      <TextInput placeholder='Amount' style={{ marginTop: 5, marginBottom: 5,  fontSize:18, width: 200, borderColor: 'gray', borderWidth: 1}}
+        onChangeText={(amount) => setAmount(amount)}
+        value={amount}/>      
+      <Button onPress={saveItem} title="Save" /> 
+      <Text style={{marginTop: 30, fontSize: 20}}>Shopping list</Text>
+      <FlatList 
+        style={{marginLeft : "5%"}}
+        keyExtractor={item => item.key} 
+        renderItem={({item}) => <View style={styles.listcontainer}><Text style={{fontSize: 18}}>{item.product}, {item.amount}</Text>
+        <Text style={{fontSize: 18, color: '#0000ff'}} onPress={() => deleteItem(item.key)}> delete</Text></View>} 
+        data={list} 
+        ItemSeparatorComponent={listSeparator} 
+      />      
     </View>
   );
 }
@@ -17,4 +77,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  listcontainer: {
+    flexDirection: 'row',
+    backgroundColor: '#fff',
+    alignItems: 'center'
+   },
 });
